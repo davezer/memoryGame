1257bbe704f1343d32dcb489e067ab5e8202ad26
Dave Oliverio <davezer27@gmail.com>
2022-06-17 15:39:50 -0400

    added a play again feature which resets the board and reshuffles the cards

--- scripts.js
+++ scripts.js
@@ -1,58 +1,66 @@
 const cards = document.querySelectorAll('.memoryCard');
 
-  let hasFlippedCard = false;
-  let lockBoard = false;
-  let firstCard, secondCard;
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
 
-  function flipCard() {
+function flipCard() {
     if (lockBoard) return;
     if (this === firstCard) return;
 
     this.classList.add('flip');
 
     if (!hasFlippedCard) {
-      hasFlippedCard = true;
-      firstCard = this;
-      return;
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
     }
 
     secondCard = this;
     lockBoard = true;
 
     checkForMatch();
-  }
+}
 
-  function checkForMatch() {
+function checkForMatch() {
     let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
     isMatch ? disableCards() : unflipCards();
-  }
+}
 
-  function disableCards() {
+function disableCards() {
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
 
     resetBoard();
-  }
+}
 
-  function unflipCards() {
-    setTimeout(() => {
-      firstCard.classList.remove('flip');
-      secondCard.classList.remove('flip');
+function unflipCards() {
+setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
 
-      resetBoard();
-    }, 1500);
-  }
+    resetBoard();
+}, 1500);
+}
 
-  function resetBoard() {
+function resetBoard() {
     [hasFlippedCard, lockBoard] = [false, false];
     [firstCard, secondCard] = [null, null];
-  }
+}
 
- (function shuffle() {
-   cards.forEach(card => {
-     let randomPos = Math.floor(Math.random() * 12);
-     card.style.order = randomPos;
-   });
- })();
+function newGame() {
+    document.querySelector('.newGame').addEventListener('click', resetBoard);
+    window.location.reload();
+    
 
-  cards.forEach(card => card.addEventListener('click', flipCard));
+}
+
+
+(function shuffle() {
+cards.forEach(card => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+});
+})();
+
+cards.forEach(card => card.addEventListener('click', flipCard));
